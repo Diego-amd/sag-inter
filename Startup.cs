879dfc8dbd8bbd2430ea7863f77dade77ac268d5
heiterpm <heiterpm@gmail.com>
2021-10-08 18:01:49 -0300

    Troquei Homes por Login

--- Startup.cs
+++ Startup.cs
@@ -35,7 +35,7 @@ namespace sag
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Login/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -50,7 +50,7 @@ namespace sag
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
