4bc183dcbad17de901dbfbfcdc83a57ae6077d41
Diego Armindo <diegoaarmindo@gmail.com>
2021-10-22 18:09:02 -0300

    Gastos Brutos Model, Controller e Repository

---
+++ Models/GastosBrutos.cs
@@ -0,0 +1,25 @@
+namespace sag.Models
+{
+    public class GastosBrutos
+    {
+        public int Id_gasto { get; set; }
+        public int CodUsuario { get; set; }
+        public float Valor { get; set; }
+        public string DataVencimento { get; set; }
+        public string DataPagamento { get; set; }
+
+        #region Foreign Key
+        public Admin Admin { get; set; }
+        #endregion
+    }
+}
+
+
+// CREATE TABLE tb_gastos_brutos(
+// 	id_gasto		int primary key identity not null,
+// 	cod_usuario		int references tb_admin not null,
+// 	valor_gasto		float not null,
+// 	data_pagamento	date,
+// 	data_vencimento	date
+// )
+// go
---
+++ Repositories/GastosBrutosDatabaseRepository.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using sag.Models;
+using sag.Repositores;
+
+namespace sag.Repositories
+{
+    public class GastosBrutosDatabaseRepository : BDContext, IGastosBrutosRepository
+    {
+        public void Create(GastosBrutos model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<GastosBrutos> Read()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public GastosBrutos Read(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(int id, GastosBrutos model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
