309948bad46bacd02b0db3b72238877166232486
Arthur Lins Beluci <arthurjbeluci@gmail.com>
2021-11-24 11:23:52 -0300

    Layout pika quase pronto, falta colocar o endereço de alguns botões e termina de formata

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,18 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/* Função de Funções Administrativas */
 
-// Write your JavaScript code.
+function funAdmLayout() {
+    document.getElementById("fun-adm-layout").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("fun-adm-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
