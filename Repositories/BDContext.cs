2bd36dc688d26f3e2bb33c62b67276942bfc4858
Arthur Lins Beluci <arthurjbeluci@gmail.com>
2021-11-26 18:58:02 -0300

    commit

--- Repositories/BDContext.cs
+++ Repositories/BDContext.cs
@@ -16,8 +16,7 @@ namespace sag.Repositores
         {
             // var context = new BDContext(); //abre a conexão
             // context.Dispose(); //fecha a conexão
-            // var strConnection = "Data Source = localhost; Integrated Security = True; User=admin; Password=A1b2c3d4e5!; Initial Catalog = db_sag";
-            var strConnection = @"Data Source=DESKTOP-C839A6U\SQLEXPRESS;Initial Catalog=db_sag;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var strConnection = "Data Source = localhost; Integrated Security = True; User=admin; Password=A1b2c3d4e5!; Initial Catalog = db_sag";
             connection = new SqlConnection(strConnection);
             connection.Open();
             Console.WriteLine("Abriu");
