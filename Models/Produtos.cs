fe3c344fb26eb6ff94a0fab80c5fba4c8c034338
Diego Armindo <diegoaarmindo@gmail.com>
2021-11-29 21:42:06 -0300

    Mensagem de validação funcionando

--- Models/Produtos.cs
+++ Models/Produtos.cs
@@ -6,11 +6,14 @@ namespace sag.Models
     {
         public int Id_produto { get; set; }
         public int Cod_usuario { get; set; }
+        
         [Required(ErrorMessage = "O campo Nome é obrigatório")]
         public string Nome { get; set; }
         public string Descricao { get; set; }
+        
         [Required(ErrorMessage = "O campo Categoria é obrigatório")]
         public string Categoria { get; set; }
+
         [Required(ErrorMessage = "O campo Valor é obrigatório")]
         public decimal Valor { get; set; }
         public int Estado { get; set; }
